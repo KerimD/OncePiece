56b2fa2b67caba5bd43a6b3106ea5e901172835b
Your Name <you@example.com>
2020-04-02 02:08:18 -0500

    y

---
+++ src/com/trees/plugin/ArmorMain.java
@@ -0,0 +1,18 @@
+package com.trees.plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArmorMain extends JavaPlugin {
+
+    // Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+    }
+    
+    
+    
+}
